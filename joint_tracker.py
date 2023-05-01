2eb947843e1ba916207af5a32cb580c08ef524df
sbb <estevaopbsantos@gmail.com>
2023-05-01 18:23:25 -0300

    n-articulated arm support

--- joint_tracker.py
+++ joint_tracker.py
@@ -1,7 +1,9 @@
+from time import sleep
+from typing import List
+
 import numpy as np
 from kaspersmicrobit import KaspersMicrobit
 from kaspersmicrobit.services.leddisplay import Image
-from time import sleep
 
 
 def get_angle(v1, v2):
@@ -11,68 +13,81 @@ def get_angle(v1, v2):
     cos_angle = dot_product / (v1_mag * v2_mag)
     angle = np.arccos(cos_angle)
     cross_product = np.cross(v1, v2)
-    # print(v1, v2, cross_product)
     if int(cross_product) < 0:
         angle = 2 * np.pi - angle
     return angle
 
 
 class JointTracker:
-    def __init__(self, arm_mb_id, forearm_mb_id) -> None:
-        self.arm_mb, self.forearm_mb = self.get_connection(
-            arm_mb_id, forearm_mb_id)
-        self.arm_vector = None
-        self.forearm_vector = None
+    def __init__(self, *microbits: str | KaspersMicrobit) -> None:
+        self.microbits = self.get_connection(microbits)
+        self.vectors: List[np.ndarray(shape=3, dtype=float)] = []
         self.gravity_north_angle = None
-        self.joint_angle = None
         # self.startup()
 
     def startup(self):
-        for image in (Image.CLOCK12, Image.CLOCK1, Image.CLOCK2, Image.CLOCK3,
+        for image in (Image.CLOCK1, Image.CLOCK2, Image.CLOCK3,
                       Image.CLOCK4, Image.CLOCK5, Image.CLOCK6, Image.CLOCK7,
                       Image.CLOCK8, Image.CLOCK9, Image.CLOCK10, Image.CLOCK11,
                       Image.CLOCK12):
             self.arm_mb.led.show(image)
             sleep(1)
         self.gravity_north_angle = get_angle(
-            self._get_magnetometer(self.arm_mb),
-            self._get_accelerometer(self.arm_mb)
+            self._get_magnetometer(self.microbits[0]),
+            self._get_accelerometer(self.microbits[0])
         )
 
     @staticmethod
-    def _get_accelerometer(mb: KaspersMicrobit):
+    def _get_accelerometer(mb: KaspersMicrobit) -> np.ndarray(shape=3, dtype=np.int16):
         data = mb.accelerometer.read()
-        return np.array([data.x, data.y, data.z])
+        return np.array([data.x, data.y, data.z], dtype=np.int16)
 
     @staticmethod
-    def _get_magnetometer(mb: KaspersMicrobit):
+    def _get_magnetometer(mb: KaspersMicrobit) -> np.ndarray(shape=3, dtype=np.int16):
         data = mb.magnetometer.read_data()
-        return np.array([data.x, data.y, data.z])
+        return np.array([data.x, data.y, data.z], dtype=np.int16)
 
     @staticmethod
-    def get_connection(arm_mb_id, forearm_mb_id):
-        mbs = []
-        for mb in (KaspersMicrobit(arm_mb_id), KaspersMicrobit(forearm_mb_id)):
-            mb.connect()
-            mbs.append(mb)
-        return mbs[0], mbs[1]
+    def get_connection(microbits: List[KaspersMicrobit | str]) -> List[KaspersMicrobit]:
+        connected_kms: List[KaspersMicrobit] = []
+        for microbit in microbits:
+            match microbit:
+                case KaspersMicrobit():
+                    microbit.connect()
+                    connected_kms.append(microbit)
+                case str():
+                    _microbit = KaspersMicrobit(microbit)
+                    _microbit.connect()
+                    connected_kms.append(_microbit)
+                case _:
+                    raise TypeError(
+                        f'Invalid type of {microbit}, it should be either KaspersMicrobit or str.')
+        return connected_kms
 
     def update(self):
-        arm_north = self._get_magnetometer(self.arm_mb)
-        forearm_north = self._get_magnetometer(self.forearm_mb)
-
-        arm_north_yz = np.array([arm_north[1], arm_north[2]])
-
-        forearm_north_yz = np.array([forearm_north[1], forearm_north[2]])
-        # forearm_north_yz = forearm_north_yz / norm(forearm_north_yz)
-
-        # Calculate the angle between the north vector and the forearm
-        theta = get_angle(forearm_north_yz, np.array([-1, 0]))
-
-        r = np.array([[1, 0, 0], [0, np.cos(theta), -np.sin(theta)],
-                     [0, np.sin(theta), np.cos(theta)]])
-        self.forearm_vector = np.dot(
-            r, np.array([0, arm_north[1], arm_north[2]]) / np.linalg.norm(arm_north_yz))
-        self.arm_vector = np.array([0, -1, 0])
-        # self.joint_angle = get_angle(self.arm_vector, self.forearm_vector)
-        # phi = np.arccos(np.dot(np.array([0, 1, 0]), vector)) * 180 / np.pi
+        vectors = [np.array([0, -1, 0])]
+        north0 = self._get_magnetometer(self.microbits[0])
+        north0_yz = np.array([north0[1], north0[2]])
+        for microbit in self.microbits[1:]:
+            northn = self._get_magnetometer(microbit)
+            northn_yz = np.array([northn[1], northn[2]])
+            theta = get_angle(northn_yz, np.array(
+                [-1, 0]))
+            r = np.array([[1, 0, 0], [0, np.cos(theta), np.sin(theta)],
+                         [0, -np.sin(theta), np.cos(theta)]])  # testar matriz de rotação no sentido oposto
+            narm_vector = np.dot(
+                r, np.array([0, north0[1], north0[2]]) / np.linalg.norm(north0_yz))
+            vectors.append(narm_vector)
+        rot_vectors = []
+        north0_xy = np.array([north0[0], north0[1]])
+        phi = get_angle(north0_xy, np.array([0, 1]))
+        rphi = np.array([[np.cos(phi), np.sin(phi), 0],
+                         [-np.sin(phi), np.cos(phi), 0],
+                         [0, 0, 1]])
+        north0_xz = np.array([north0[0], north0[2]])
+        gama = get_angle(north0_xz, np.array([0, 1]))
+        rgama = np.array([[np.cos(gama), 0, -np.sin(gama)],
+                          [0, 1, 0],
+                          [np.sin(gama), 0, np.cos(gama)]])
+        for vector in vectors:
+            rot_vectors.append(np.dot(rgama, np.dot(rphi, vector)))
