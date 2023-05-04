c19a9dfb67879e65ba996270966c15b42188b00d
sbb <estevaopbsantos@gmail.com>
2023-05-04 01:57:09 -0300

    comments

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from animate_joint import JointAnimation
 if __name__ == '__main__':
     joint_tracker = JointTracker('C3:B0:42:88:FE:07', 'F6:8C:51:58:97:63')
     joint_animation = JointAnimation(
-        joint_tracker, [1, 1], frames=5000, xyz_lim=[[-1.5, -1.5]] * 3)
+        joint_tracker, [1, 1], frames=5000, xyz_lim=[[-2, 2]] * 3)
     joint_animation.animate()
