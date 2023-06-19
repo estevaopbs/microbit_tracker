baa1bb8e85002c3a57ad43b8c311d010d66dd739
sbb <estevaopbsantos@gmail.com>
2023-06-19 00:11:25 -0300

    state

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from joint_tracker import JointTracker
 from animate_joint import JointAnimation
 
 if __name__ == "__main__":
-    joint_tracker = JointTracker("C3:B0:42:88:FE:07", "F6:8C:51:58:97:63", 0.5, False)
+    joint_tracker = JointTracker(
+        "C3:B0:42:88:FE:07", "F6:8C:51:58:97:63", calculation_time=0.5, record=False
+    )
     joint_animation = JointAnimation(
         joint_tracker, [1, 1], frames=5000, xyz_lim=[[-2, 2]] * 3
     )
