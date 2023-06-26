8a9fcb99ce6e3ab53bd8b5613e9fdb471e4220a6
sbb <estevaopbsantos@gmail.com>
2023-06-26 00:29:22 -0300

    code

--- animate_joint.py
+++ animate_joint.py
@@ -2,16 +2,18 @@ import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
 from joint_tracker import JointTracker
+import matplotlib.patches as patches
 
 
 class JointAnimation:
     def __init__(self, joint_tracker: JointTracker, lenghts, frames, xyz_lim):
         self.joint_tracker = joint_tracker
         self.lengths = lenghts
+        self.articulation_diameter = 0.3
         self.frames = frames
         self.frame = 0
         self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(111, projection="3d")
+        self.ax = self.fig.add_subplot(111)
         self.xyz_lim = xyz_lim
         self.text = self.ax.text2D(
             0.85,
@@ -28,16 +30,57 @@ class JointAnimation:
         self.ax.clear()
         self.ax.set_xlim(self.xyz_lim[0])
         self.ax.set_ylim(self.xyz_lim[1])
-        self.ax.set_zlim(self.xyz_lim[2])
-        last_point = np.array([0, 0, 0])
-        for vector, length in zip(self.joint_tracker.state.vectors, self.lengths):
-            self.ax.quiver(*last_point, *vector * length)
-            last_point = last_point + vector * length
+        # self.ax.set_zlim(self.xyz_lim[2])
+        # last_point = np.array([0, 0, 0])
+        rtheta = np.array(
+            [
+                [1, 0, 0],
+                [0, np.cos(np.pi), np.sin(np.pi)],
+                [0, -np.sin(np.pi), np.cos(np.pi)],
+            ]
+        )
+        # norm_vec = self.joint_tracker.state.vectors[0] / np.linalg.norm(
+        #    self.joint_tracker.state.vectors[0]
+        # )
+        # norm_rot_vec = np.dot(rtheta, norm_vec)
+        # rectangle_origin = norm_rot_vec * self.articulation_diameter / 2
+        # self.ax.add_patch(
+        #    patches.Rectangle(
+        #        rectangle_origin,
+        #        self.lengths[0] - self.articulation_diameter,
+        #        self.articulation_diameter,
+        #        angle=self.joint_tracker.state.angles[0] + ((3 * np.pi) / 2),
+        #        rotation_point=(0, 0),
+        #    )
+        # )
+        articulation_origin = np.zeros(2)
+        for vector, angle, length, n in zip(
+            self.joint_tracker.state.vectors,
+            self.joint_tracker.state.angles,
+            self.lengths,
+            range(len(self.lengths)),
+        ):
+            self.ax.add_patch(
+                patches.Circle(
+                    articulation_origin, radius=self.articulation_diameter / 2
+                )
+            )
+            norm_vec = vector / np.linalg.norm(vector)
+            norm_rot_vec = np.dot(rtheta, norm_vec)
+            rectangle_origin = (
+                (norm_rot_vec + norm_vec) * self.articulation_diameter / 2
+            )
+            self.ax.add_patch(
+                patches.Rectangle(
+                    rectangle_origin,
+                    length - self.articulation_diameter,
+                    self.articulation_diameter,
+                    angle=angle + 270 if n == 0 else angle,
+                    rotation_point=articulation_origin,
+                )
+            )
+            articulation_origin += norm_vec * length
         self.ax.quiver(*[0, 0, 0], *self.joint_tracker.fixed_gravity, color="red")
-        text = ""
-        for n, angle in enumerate(self.joint_tracker.state.angles):
-            text += f"angle_{n} = {angle}\n"
-        self.text.set_text("")
         return
 
     def animate(self):
